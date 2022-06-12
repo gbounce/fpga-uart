8ce25168eb1f1ac809b50d03a7dc1592608db302
Ryan <rrobertson@gmail.com>
2022-06-11 17:29:06 -0700

    headers

--- src/uart_rx.sv
+++ src/uart_rx.sv
@@ -1,5 +1,5 @@
 /*
-Copyright (C) {{ year }}  {{ organization }}
+Copyright (C) 2022 Ryan Robertson <rrobertson@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
